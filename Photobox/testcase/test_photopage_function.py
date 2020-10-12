203dff31c7c3a7e6e248e3e44f275066b2026664
jimmylinz <jimmylin928@163.com>
2020-10-12 14:45:50 +0800

    test

--- Photobox/testcase/test_photopage_function.py
+++ Photobox/testcase/test_photopage_function.py
@@ -16,26 +16,26 @@ class TestPhotopageFunction():
         lastnumber = self.login.pdadd_picture_video().select_album().pd_select_photo().get_file_number()
         assert lastnumber == number + 1
 
-    #隐藏文件
-    def test_hide_pdfile(self):
-        number = self.login.get_file_number()
-        lastnumber = self.login.pdadd_file().pd_select_file().get_file_number()
-        assert lastnumber == number + 1
-
-    #取消隐藏
-    def test_unhide_pdfile(self):
-        number = self.login.get_file_number()
-        lastnumber = self.login.pd_unhide()
-        assert lastnumber == number - 1
-
-    #移动文件
-    def test_move_pdfile(self):
-        firstto_number = self.login.get_file_number()
-        last_number = self.login.pd_move()
-        assert last_number == firstto_number - 1
-
-    #删除文件
-    def test_delete_pdfile(self):
-        firstto_number = self.login.get_file_number()
-        last_number = self.login.pd_move()
-        assert last_number == firstto_number - 1
+    # #隐藏文件
+    # def test_hide_pdfile(self):
+    #     number = self.login.get_file_number()
+    #     lastnumber = self.login.pdadd_file().pd_select_file().get_file_number()
+    #     assert lastnumber == number + 1
+    #
+    # #取消隐藏
+    # def test_unhide_pdfile(self):
+    #     number = self.login.get_file_number()
+    #     lastnumber = self.login.pd_unhide()
+    #     assert lastnumber == number - 1
+    #
+    # #移动文件
+    # def test_move_pdfile(self):
+    #     firstto_number = self.login.get_file_number()
+    #     last_number = self.login.pd_move()
+    #     assert last_number == firstto_number - 1
+    #
+    # #删除文件
+    # def test_delete_pdfile(self):
+    #     firstto_number = self.login.get_file_number()
+    #     last_number = self.login.pd_move()
+    #     assert last_number == firstto_number - 1
